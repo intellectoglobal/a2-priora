16c988ec00999ae040f625e35852d53c9fab38bb
RAHUL R <rahulsquads@gmail.com>
2023-02-23 03:04:59 +0530

    added admin login page

--- backend-django/course_register_fe/models.py
+++ backend-django/course_register_fe/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+payment_status = (
+    ('None':'None'),
+    ('Paid':'Paid'),
+    ('Not Yet':'Not Yet'),
+)
+
+
+class CouurseRegister(models.Model):
+    register_id=models.AutoField(primary_key=True)
+    selected_course=models.ForeignKey()
+    applicant_name=models.CharField(max_length=20,blank=False)
+    contact_no=models.IntegerField(max_digits=10,blank=False)
+    email=models.EmailField(blank=False)
+    alternative_email=models.EmailField()
+    address=models.CharField(max_length=50)
+    postal_code=models.IntegerField()
+    FIN_NRIC_passport_no =models.IntegerField(max_digits=20)
+    company_name=models.CharField(max_length=30)
+    membership_type=models.BooleanField(default=False)
+    membership_n0=models.IntegerField()
+    payment_status=models.CharField()
+    payment_receipt=models.
+   
