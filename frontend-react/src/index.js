95c5806222baa1f3a3e0d005b5592ac89575b0c4
RAHUL R <rahulsquads@gmail.com>
2023-02-24 01:58:25 +0530

    added admin route

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -4,17 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter  } from 'react-router-dom';
+import AdminRoute from './AdminRoute';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+            <AdminRoute/>
     <BrowserRouter>
-      <App />
+            <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
