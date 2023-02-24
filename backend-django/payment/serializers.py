a91df0e8ba39dd290c68b4c620196e72de64077a
Adam <adamofficial0145@gmail.com>
2023-02-25 02:18:55 +0530

    Added views.py for apps

--- backend-django/payment/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from . models import Payment
-
-class Payment(serializers.ModelSerializer):
-    class Meta :
-        model = Payment
-        fields = "__all__"
--- backend-django/schedule_courses_be/views.py
+++ backend-django/schedule_courses_be/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .serializers import ScheduleCourseSerializer
+from .models import ScheduleCourse
+from rest_framework.views import APIView
+
 
 # Create your views here.
-#test
+
+class ScheduleCourseView(APIView):
+    serializer_class = ScheduleCourseSerializer
+
+    def post(self, request, *args, **kwargs):
+            data = request.data
+            serializer = ScheduleCourseSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+            return Response({"Response" : "Course is successfully registered.","data" : serializer.data})
