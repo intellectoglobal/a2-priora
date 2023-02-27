57d25cf41c8f6855f191394e8a9c44541feded4f
Rahul R <109589036+rahulr001@users.noreply.github.com>
2023-02-27 19:09:34 +0530

    Update App.js

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,25 +1,22 @@
 import "./App.css";
-import Navbar from "./Components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
-import Home from "./Components/Home/Home";
-import Footer from "./Components/Footer/Footer";
-import Course from "./Components/Course/Course";
+import Home from "./Components/Home/Home.tsx";
+import Course from "./Components/Course/Course.tsx";
 import Login from "../src/AdminPage/Login/Login";
 
 function App() {
   return (
     <div className="App">
-       
-      <Navbar />
-      <Routes>
+    <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="admin" element={<Login />} />
         {/* <Route path="/about" element={<About/>}/> */}
         {/* <Route path="/careers" element={<Careers/>}/> */}
         <Route path="/course" element={<Course />} />
       </Routes>
-      <Footer />
     </div>
   );
 }
 
 export default App;
+
