dccd1cfeb37bc3bf5a5bb0f7540640365b1c6e58
Rahul R <109589036+rahulr001@users.noreply.github.com>
2023-02-27 19:13:02 +0530

    Update App.js

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import 'antd/dist/reset.css'
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home.tsx";
 import Course from "./Components/Course/Course.tsx";
