1b7d1823515f6c1db2690d4a65390d31dd986feb
Thiruvenkatraja <venkat.tv.raja@gmail.com>
2023-03-01 03:23:41 +0530

    changes made

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -3,8 +3,9 @@ import "antd/dist/reset.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home.tsx";
 import Course from "./Components/Course/Course.tsx";
-import Login from "../src/AdminPage/Login/Login";
+import Login from "./AdminPage/Login/Login";
 import SponserForm from "./Components/SponserForm/SponserForm.tsx";
+import CourseTable from "./Components/CourseTable/CourseTable.tsx";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         {/* <Route path="/careers" element={<Careers/>}/> */}
         <Route path="/course" element={<Course />} />
         <Route path="/sform/:course" element={<SponserForm/>} />
+        <Route path="/coursetable" element={<CourseTable/>} />
       </Routes>
     </div>
   );
