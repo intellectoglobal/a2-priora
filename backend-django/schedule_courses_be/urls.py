dd234aebd513863c2b798f8dd7db2640774004a7
RAHUL R <rahulsquads@gmail.com>
2023-02-28 23:39:22 +0530

    minor correction in urls.py

--- backend-django/schedule_courses_be/urls.py
+++ backend-django/schedule_courses_be/urls.py
@@ -3,6 +3,6 @@ from .views import ScheduleCourseView
 
 urlpatterns = [
     path('schedule_course', ScheduleCourseView.as_view()),
-    path('schedule_course/<int:id>', ScheduleCourseView.as_view()),
+    path('scheduled_course/<int:id>', ScheduleCourseView.as_view()),
 
 ]
