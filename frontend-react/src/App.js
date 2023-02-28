ccb374c01726344702a9626f5a6235d8d1f5e251
RAHUL R <rahulsquads@gmail.com>
2023-02-28 22:07:52 +0530

    added admin home

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -3,15 +3,17 @@ import "antd/dist/reset.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home.tsx";
 import Course from "./Components/Course/Course.tsx";
-import Login from "../src/AdminPage/Login/Login";
+import Login from "./AdminPage/Login/Login.tsx";
 import SponserForm from "./Components/SponserForm/SponserForm.tsx";
+import AdminHome from "./AdminPage/Home/AdminHome.tsx"
 
 function App() {
   return (
     <div className="App">
     <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="admin" element={<Login />} />
+        <Route path="/admin" element={<Login />} />
+        <Route path="/adminHome" element={<AdminHome />} />
         {/* <Route path="/about" element={<About/>}/> */}
         {/* <Route path="/careers" element={<Careers/>}/> */}
         <Route path="/course" element={<Course />} />
