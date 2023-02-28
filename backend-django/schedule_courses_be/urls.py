a7e2da4822a508463adbc93862eb17422deefc06
Adam <adamofficial0145@gmail.com>
2023-02-28 22:52:11 +0530

    Added urls.py

--- backend-django/schedule_courses_be/urls.py
+++ backend-django/schedule_courses_be/urls.py
@@ -1,6 +1,8 @@
-
 from django.urls import path
+from .views import ScheduleCourseView
 
 urlpatterns = [
+    path('schedule_course', ScheduleCourseView.as_view()),
+    path('schedule_course/<int:id>', ScheduleCourseView.as_view()),
 
 ]
