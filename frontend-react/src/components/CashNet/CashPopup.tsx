931f7c60c53b6eb4bdf74465e43965143834a4df
Isaacgithub1 <113463415+Isaacgithub1@users.noreply.github.com>
2023-03-02 02:27:13 +0530

    components added

--- frontend-react/src/components/CashNet/CashPopup.tsx
+++ frontend-react/src/components/CashNet/CashPopup.tsx
@@ -1,18 +1,45 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { Form,Input} from "antd";
 
+import Ripples from "react-ripples";
 function CashPopup() {
   return (
-    <div>
+    <div className='cashpopup-container'>
+      <div className="cashpopup-content">
       <div className='Cashpopup'>
       <h3>Upload Screen Shot</h3>
-      
-       
-      </div>
+     </div>
       <div className='Cashpopup1'>
-      <h3>Email:</h3>
+      <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "The email is required.",
+                },
+                {
+                  pattern:
+                    /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/,
+                  message: "Email is not valid.",
+                },
+              ]}
+            >
+              <Input
+                placeholder="E-mail:*"
+                name="email"
+                // value={values.email}
+                // onChange={handleChange}
+                style={{
+                  borderRadius: "5px",
+                  border: "1px solid #000000",
+                  width: "12rem",
+                }}
+              />
+            </Form.Item>
       </div>
       <div className='Cashpopup2'>
-      <h3>Preview Area :</h3>
+      <h3>Preview Area:</h3>
       </div>
       <div className="button-Backword">
           <Ripples>
@@ -22,6 +49,7 @@ function CashPopup() {
               </button>
             </Link>
           </Ripples>
+          </div>
           </div>
     </div>
   )
