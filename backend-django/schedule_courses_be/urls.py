58de699e27da6237db9ac69cba63bd8622df38a6
mohammedadamdev <110044593+mohammedadamdev@users.noreply.github.com>
2023-03-02 20:58:22 +0530

    updated urls, serializers and models

--- backend-django/schedule_courses_be/urls.py
+++ backend-django/schedule_courses_be/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import ScheduleCourseView
 
+# Creating a url for the view.
 urlpatterns = [
     path('schedule_course', ScheduleCourseView.as_view()),
-    path('scheduled_course/<int:id>', ScheduleCourseView.as_view()),
+    path('scheduled_course', ScheduleCourseView.as_view()),
 
 ]
