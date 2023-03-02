296608457afec79b264a849b34f1c78b0af68f83
mohammedadamdev <110044593+mohammedadamdev@users.noreply.github.com>
2023-03-03 01:22:06 +0530

    minor changes in schedule course url

--- backend-django/schedule_courses_be/urls.py
+++ backend-django/schedule_courses_be/urls.py
@@ -4,6 +4,6 @@ from .views import ScheduleCourseView
 # Creating a url for the view.
 urlpatterns = [
     path('schedule_course', ScheduleCourseView.as_view()),
-    path('scheduled_course', ScheduleCourseView.as_view()),
+    path('upcoming_scheduled_course', ScheduleCourseView.as_view()),
 
 ]
